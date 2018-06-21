1e31d94ae097eb006c7bfc4c1a664c5fcc5a8c6a
ranggull <39206418+ranggull@users.noreply.github.com>
2018-06-21 16:50:22 -0500

    update1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int hoursWorked = 0;
+		private Person[] staffList; 
+		staffList = new PaidAdvisor[3];
+		staffList[0] = new PaidAdvisor("Sterling", "Archer", hoursWorked);
+		staffList[1] = new PaidAdvisor("Lana", "Kane", hoursWorked);
+		staffList[2] = new PaidAdvisor("Cyril", "Figgis", hoursWorked);
+		
 		PaidAdvisor staff = new PaidAdvisor();
 		
 		staff.payroll();
