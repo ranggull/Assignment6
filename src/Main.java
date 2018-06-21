b4675a07f07df6856be605485eaf4d71629f1b3c
ranggull <39206418+ranggull@users.noreply.github.com>
2018-06-21 16:51:12 -0500

    next

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 
+	int hoursWorked = 0;
+	private Person[] staffList; 
+	staffList = new PaidAdvisor[3];
+	
 	public static void main(String[] args) {
 		
-		int hoursWorked = 0;
-		private Person[] staffList; 
-		staffList = new PaidAdvisor[3];
 		staffList[0] = new PaidAdvisor("Sterling", "Archer", hoursWorked);
 		staffList[1] = new PaidAdvisor("Lana", "Kane", hoursWorked);
 		staffList[2] = new PaidAdvisor("Cyril", "Figgis", hoursWorked);
