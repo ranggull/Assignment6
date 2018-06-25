5ce6b96a4252ca85439d5bb2e0a5225a3126036b
ranggull <39206418+ranggull@users.noreply.github.com>
2018-06-24 20:17:25 -0500

    6/24 8:17

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 				
-		
+		Person p = new Person(Sterling, Archer);
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("How many hours did you work?");
+		System.out.println("How many hours did " + p.getFirstName() + p.getLastName() + "work?");
+		int hours = 0;
+		hours = scan.nextInt();
+		p.setHours(hours);
+		System.out.println("How many of those hours were special hours?");
+		int specialHours = 0;
+		specialHours = scan.nextInt();
+		System.out.println(p.getFirstName() + p.getLastName() + " earned " + p.toString(payCheck) + " dollars.");
 		
 		
 	}
